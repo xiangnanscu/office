9d641f800066c802c98b6da70baf6af44ff9ffc4
xiangnan <280145668@qq.com>
2017-10-31 12:06:25 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +6,27 @@ using System.Text.RegularExpressions;
 using Word = Microsoft.Office.Interop.Word;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.IO;
-//using System.CodeDom.Compiler;
-//using System.CodeDom;
+
 
 namespace ConsoleApp1
 {
-    public delegate void DelegateDocument(Word.Document doc);
-    public delegate void DelegateWorkbook(Excel.Workbook book);
-
 
     class Program
     {
-        static string cd = Directory.GetCurrentDirectory();
-        static string qfd = cd + "/签发单.doc";
+        static Word.Application word;
+        static Excel.Application excel;
         static Dictionary<string, string> holder;
+        static Boolean isDoc = false;
+        static string cd = Directory.GetCurrentDirectory();
+        static string qianfadan;
+        static HashSet<string> fileTypes = new HashSet<string>() { ".doc", ".docx", ".xls", ".xlsx" };
+        static string patternChaosong = @"^\s*抄送[:：]([^:：\n\r。.]+)[。.]?\s*";
+        static string patternZhuSong = @"^\s*([^:：\s]+)[:：]\s*$";
+        static string patternWenHao = @"^\s*(?<代字>\w+)〔(?<年份>\d{4})〕(?<序号>\d*)号\s*$";
+        static string patternDate = @"^\s*(\d+年\d+月\d+日)\s*$";
 
-        static Dictionary<string, string> MakeHolder() {
+        static Dictionary<string, string> MakeHolder()
+        {
             return new Dictionary<string, string> {
                 {"代字","" },
                 {"年份","" },
@@ -32,64 +37,29 @@ namespace ConsoleApp1
                 {"抄送","" },
             };
         }
-        static void ProcessWord(string path, DelegateDocument handler)
+
+        private static void FindQianFaDan()
         {
-            Word.Application word = new Word.Application();
-            try {             
-                var doc = word.Documents.Open(path);
-                handler(doc);
-            } finally {
-                word.Quit();
-            }
-            
-        }
-        static void ProcessQfd(string name)
-        {
-            Word.Application word = new Word.Application();
-            try
+            var candidates = Directory.GetFiles(cd, "*.*", SearchOption.AllDirectories)
+                .Where(file => Path.GetFileNameWithoutExtension(file) == "签发单" && fileTypes.Contains(Path.GetExtension(file)))
+                .ToList();
+            if (candidates.Count == 0)
             {
-                var doc = word.Documents.Open(qfd);
-                foreach (KeyValuePair<string, string> kvp in holder) {
-                    Word.Find findObject = word.Application.Selection.Find;
-                    findObject.Text = "{"+kvp.Key+"}";
-                    findObject.Replacement.Text = kvp.Value;
-                    findObject.Execute(Replace: Word.WdReplace.wdReplaceAll);
+                throw new Exception("没有找到签发单模板文件");
+            }
+            else {
+                qianfadan = candidates[0];
+                var t = Path.GetExtension(qianfadan);
+                if (t == ".doc" || t == ".docx") {
+                    isDoc = true;
                 }
-                doc.SaveAs(FileName: cd + $"/{name}.doc");
+                Print("找到签发单模板文件:", qianfadan);
             }
-            finally
-            {
-                word.Quit();
-            }
-
         }
-        static void ProcessExcel(string path, DelegateWorkbook handler)
-        {
-            Excel.Application excel = new Excel.Application();
-            try
-            {               
-                var xls = excel.Workbooks.Open(path);
-                handler(xls);
-            }
-            finally
-            {
-                excel.Quit();
-            }
-
-        }
-
 
 
         private static string ToLiteral(string input)
         {
-            //using (var writer = new StringWriter())
-            //{
-            //    using (var provider = CodeDomProvider.CreateProvider("CSharp"))
-            //    {
-            //        provider.GenerateCodeFromExpression(new CodePrimitiveExpression(input), writer, null);
-            //        return writer.ToString();
-            //    }
-            //}
             return input;
         }
 
@@ -100,12 +70,13 @@ namespace ConsoleApp1
                 Console.WriteLine(ToLiteral(item));
             }
         }
-        static void GetWenHao(Word.Document document) {
-            var pattern = @"(?<代字>\w+)〔(?<年份>\d{4})〕(?<序号>\d+)号";
+        static void GetWenHao(Word.Document document)
+        {
+            
             foreach (Word.Paragraph p in document.Paragraphs)
             {
                 var r = p.Range;
-                var m = Regex.Match(r.Text, pattern);
+                var m = Regex.Match(r.Text, patternWenHao);
                 if (m.Success)
                 {
                     holder["代字"] = m.Groups["代字"].Value;
@@ -114,116 +85,185 @@ namespace ConsoleApp1
                     return;
                 }
             }
-            Print("警告:没有找到文号");
+            Print("警告:没有找到文号，请确保格式类似“江人社发〔2017〕1号”");
         }
         static void GetBiaoTi(Word.Document document)
         {
+            var find = false;
             foreach (Word.Paragraph p in document.Paragraphs)
             {
                 var r = p.Range;
                 if (r.Font.Name == "方正小标宋简体" && r.Font.Size == 22)
                 {
+                    find = true;
                     holder["标题"] = holder["标题"] + Regex.Replace(r.Text, "[\n\r ]", "");
                 }
             }
+            if (!find)
+            {
+                Print("警告：没有找到标题，请确保字体为“方正小标宋简体”，字号为二号");
+            }
+
         }
         static void GetZhuSong(Word.Document document)
         {
-            var pattern = @"^\s*([^:：\s]+)[:：]\s*$";
-            
+
             foreach (Word.Paragraph p in document.Paragraphs)
             {
                 var r = p.Range;
-                var m = Regex.Match(r.Text, pattern);
-                if (m.Success &&
-                    r.Font.Name == "仿宋_GB2312" && r.Font.Size == 16 
-                    && p.FirstLineIndent == 0
-                  )
+                var m = Regex.Match(r.Text, patternZhuSong);
+                if (m.Success && r.Font.Name == "仿宋_GB2312" && r.Font.Size == 16
+                    && p.FirstLineIndent == 0 )
                 {
                     holder["主送"] = m.Groups[1].Value;
                     return;
                 }
             }
-            Print("警告:没有找到主送机关");
+            Print("警告:没有找到主送机关，请确保字体为“仿宋_GB2312”，字号为三号，无首行缩进");
         }
         static void GetDate(Word.Document document)
         {
-            var pattern = @"^\s*(\d+年\d+月\d+日)\s*$";
-
+            var find = false;
             foreach (Word.Paragraph p in document.Paragraphs)
             {
                 var r = p.Range;
-                var m = Regex.Match(r.Text, pattern);
+                var m = Regex.Match(r.Text, patternDate);
                 if (m.Success && r.Font.Name == "仿宋_GB2312" && r.Font.Size == 16)
                 {
                     holder["成文日期"] = m.Groups[1].Value;
+                    find = true;
                 }
+            }
+            if (!find)
+            {
+                Print("警告：没有找到成文日期，请确保格式类似于“2017年10月20日”，字体为“仿宋_GB2312”，字号为三号");
             }
         }
         static void GetChaoSong(Word.Document document)
         {
-            var pattern = @"^\s*抄送[:：]([^:：\s]+)\s*$";
-
+          
             foreach (Word.Paragraph p in document.Paragraphs)
             {
                 var r = p.Range;
-                var m = Regex.Match(r.Text, pattern);
+                var m = Regex.Match(r.Text, patternChaosong);
                 if (m.Success && r.Font.Name == "仿宋_GB2312" && r.Font.Size < 16)
                 {
                     holder["抄送"] = m.Groups[1].Value;
                     return;
                 }
             }
+            Print("警告：没有找到抄送机关，请确保格式类似于“抄送：县委组织部”，字体为“仿宋_GB2312”，字号小于三号");
         }
-        static void HandleFile(Word.Document document)
+        static void HandleFile(Word.Document doc)
         {
-            GetWenHao(document);
-            GetBiaoTi(document);
-            GetZhuSong(document);
-            GetDate(document);
-            GetChaoSong(document);
-            document.Close(false);
+            GetWenHao(doc);
+            GetBiaoTi(doc);
+            GetZhuSong(doc);
+            GetDate(doc);
+            GetChaoSong(doc);
+            doc.Close(false);
         }
-
-
-
-        static void Work() {
+        static void Work()
+        {
             var pat = new Regex(@"^[^~]+\.docx?$");
             var docs = Directory.GetFiles(cd, "*.*", SearchOption.AllDirectories)
                 .Where(file => pat.IsMatch(file))
                 .ToList();
+            Print("docs.Count", docs.Count.ToString());
+            if (docs.Count==0)
+            {
+                Print("警告：没有找到word文件");
+            }
             foreach (string docPath in docs)
             {
-                if (docPath.Contains("签发单"))
+                if (Path.GetFileNameWithoutExtension(docPath).Contains("签发单"))
                 {
                     continue;
-                }
+                } 
                 Print("处理文件:" + docPath);
                 holder = MakeHolder();
-                ProcessWord(docPath, new DelegateDocument(HandleFile));
-                ProcessQfd($"签发单-{(holder["代字"])}-{(holder["年份"])}-{(holder["序号"])}号");
-
+                word = new Word.Application();
+                try
+                {
+                    var doc = word.Documents.Open(docPath);
+                    GetWenHao(doc);
+                    GetBiaoTi(doc);
+                    GetZhuSong(doc);
+                    GetDate(doc);
+                    GetChaoSong(doc);
+                    doc.Close(false);
+                }
+                finally
+                {
+                    word.Quit();
+                }            
+                var name = $"签发单-{(holder["代字"])}-{(holder["年份"])}-{(holder["序号"])}号";
+                ProcessQianFaDan(name);
             }
-
         }
-        static void Test()
+        static void ProcessQianFaDan(string name)
         {
+            if (isDoc) {
+                word = new Word.Application();
+                try
+                {
 
+                    var doc = word.Documents.Open(qianfadan);
+                    foreach (KeyValuePair<string, string> kvp in holder)
+                    {
+                        Word.Find findObject = word.Application.Selection.Find;
+                        findObject.Text = "{" + kvp.Key + "}";
+                        findObject.Replacement.Text = kvp.Value;
+                        findObject.Execute(Replace: Word.WdReplace.wdReplaceAll);
+                    }
+                    doc.SaveAs(FileName: cd + $"/{name}.doc");
+                }
+                finally
+                {
+                    word.Quit();
+                }
+            } else {
+                Excel.Application excel = new Excel.Application();
+                try
+                {
+                    excel.DisplayAlerts = false;   
+                    Excel.Workbook book = excel.Workbooks.Open(qianfadan);
+                  
+                    Excel.Range r = book.ActiveSheet.UsedRange;
+                    
+                    foreach (KeyValuePair<string, string> kvp in holder)
+                    {
+                        r.Replace("{" + kvp.Key + "}", kvp.Value);
+                    }
+                    book.SaveAs(cd + $"/{name}.xls");
+                }
+                finally
+                {
+                    excel.DisplayAlerts = true;
+                    excel.Quit();
+                    
+                }
+            }
         }
         static void Main(string[] args)
         {
             try
             {
                 Print("------高大上的签发单生成程序开始^_^------");
+                FindQianFaDan();
                 Work();
+            }
+            catch (Exception ex)
+            {
+                Print("出现异常：", ex.ToString());
             }
             finally
             {
-                
-                Print("------程序结束, 按任意键退出------");
+
+                Print("------程序结束, 按任意键退出-------------");
                 Console.Read();
             }
-  
+
 
         }
     }
