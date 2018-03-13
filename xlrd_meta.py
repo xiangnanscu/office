d1fae74f5a7a052dbe2cc3027a2edb43030b3215
xiangnan <280145668@qq.com>
2018-03-13 17:52:15 +0800

    Update xlrd_meta.py

--- xlrd_meta.py
+++ xlrd_meta.py
@@ -2,22 +2,31 @@ import os
 import sys
 import re
 import collections
+
 import xlrd
 
 join = os.path.join
+
 p = join(os.getcwd(), 'aa.xls')
+
+
 ret = collections.OrderedDict()
 data = xlrd.open_workbook(p)
+
+
 table = data.sheets()[0] 
 
 
+
+    
+
 for c in range(table.ncols):
-	name = table.cell(0, c).value
-	if re.match(r'^[^a-z]+$', name):
-		ret[name] = s = set()
-		for r in range(1, table.nrows):
-			s.add(table.cell(r, c).value)
+    name = table.cell(0, c).value
+    if re.match(r'^[^a-z]+$', name):
+        ret[name] = s = set()
+        for r in range(1, table.nrows):
+            s.add(table.cell(r, c).value)
 
 for k, v in ret.items():
-	if len(v)<100:
-		print(k,v)
+    if len(v)<100:
+        print(k,v)
